35fab52a1577fb6aa47cf41eacba5541d0572fcd
Bryan Ramos <bryan@ramos.codes>
2024-07-11 16:29:34 -0400

    finished exercise 1-13

--- exercises/Ch1/1-13.c
+++ exercises/Ch1/1-13.c
@@ -3,28 +3,32 @@
 int main()
 {
     int c, in_word, word_length;
+    char word[100];
 
     in_word = 0;
     word_length = 0;
 
     while ((c = getchar()) != EOF) {
-        if (c != ' ' && c != '\t' && c != '\n') {
-            in_word = 1;
-            word_length = 1;
-        }
-
-        while (in_word) {
-            char word[100];
-            int idx = 0;
-            word[idx] = c;
+        if (c != ' ' && c != '\t' && c != '\n' && c != '\0') {
+            if (!in_word) {
+                in_word = 1;
+                word_length = 0;
+            }
+            word[word_length] = c;
             word_length++;
+        } else if (c == ' ' || c == '\t' || c == '\n' || c == '\0' && in_word) {
+            in_word = 0;
+            word[word_length] = '\0';
+            printf("%-7d |", word_length);
+            for (int i = 0; i < word_length; i++) {
+                printf("*");
+            }
+            printf("\n");
 
-            if (c == ' ' || c == '\t' || c == '\n' || c == '\0') {
-                in_word = 0;
-                printf('Word: %s |  Word Length: %d', word, word_length);
+            for (int i = 0; i < word_length; i++) {
+                word[i] = '\0';
             }
         }
     }
-
     return 0;
 }
