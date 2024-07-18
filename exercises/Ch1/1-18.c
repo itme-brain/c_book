0673eb120b60a003ef0356fb231103ef15b4ee23
Bryan Ramos <bryan@ramos.codes>
2024-07-18 17:08:32 -0400

    Not happy with this solution to 1-18 but time to move on

--- exercises/Ch1/1-18.c
+++ exercises/Ch1/1-18.c
@@ -6,14 +6,52 @@
     and to delete entirely blank lines
 */
 
+#define MAXLINE     1000
+#define TRUE        1
+#define FALSE       0
+
+int check_blank(int s, int trailing);
+int check_eol(int s);
+
 int main()
 {
     int c;
+    int i = 0;
+    int trailing = TRUE;
+    char line[MAXLINE];
 
     while ((c = getchar()) != EOF) {
-        char line;
-        if (c == '.' || c == '!' || c == '?') {
-
+        trailing = check_blank(c, trailing);
+        if (!trailing) {
+            line[i] = c;
+            i++;
+            trailing = check_eol(c);
         }
     }
+
+    line[i] = '\0';
+
+    if (i > 0) {
+        printf("%s", line);
+    } else if (i <= 0) {
+        printf("Empty input");
+    }
+
+    return 0;
+}
+
+int check_blank(int s, int trailing)
+{
+    if ((s == ' ' || s == '\t' || s == '\n') && trailing) {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+int check_eol(int s)
+{
+    if (s == '\n') {
+        return TRUE;
+    }
+    return FALSE;
 }
