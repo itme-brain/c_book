56ec57a39e6093fe3e95a731ef316e83c6f951ac
Bryan Ramos <bryan@ramos.codes>
2024-07-19 22:04:25 -0400

    1-21 done

--- exercises/Ch1/1-21.c
+++ exercises/Ch1/1-21.c
@@ -2,24 +2,41 @@
 
 /*
     Page 34:
-    Write a program `entab` that replaces strings of blanks by the minimum number of tabs and blanks to achieve the same spacing.
+    Write a program `entab` that replaces strings of blanks by the minimum var of tabs and blanks to achieve the same spacing.
     Use the same tab stops as for `detab`. When either a tab or a single blank would suffice to reach a tab stop, which should be given preference.
 */
 
 #define MAXLINE     1000
 #define TAB         4
 
-int entab();
-
 int main()
 {
-    int c, i;
     char line[MAXLINE];
 
-    i = 0;
-    while((c = getchar()) != EOF) {
-        if (c == ' ') {
+    int c;
+    int counter = 0;
+    int i = 0;
 
+    while((c = getchar()) != EOF) {
+        line[i++] = c;
+
+        if (c == ' ') {
+            counter++;
+        } else {
+            counter = 0;
+        }
+
+        if (counter == TAB) {
+            i -= TAB;
+            line[i++] = '\t';
+            counter = 0;
+        }
+
+        if (c == '\n') {
+            line[i] = '\0';
+            printf("%s", line);
+            counter = 0;
+            i = 0;
         }
     }
     return 0;
