a1624ee6978f2ad390a176c023321bee1a60f242
Bryan Ramos <bryan@ramos.codes>
2024-07-29 08:46:01 -0400

    1-22 done

--- exercises/Ch1/1-22.c
+++ exercises/Ch1/1-22.c
@@ -8,19 +8,29 @@
     the specified column.
 */
 
-#define MAXLINE 1000
-#define COLUMN  20
+#define MAXLINE     1000
+#define MAX_COLUMN  5
 
 int c;
 int i = 0;
-int j = 0;
 char line[MAXLINE];
 
 int main()
 {
+    int count = 0;
     while((c = getchar()) != EOF) {
+        if (count >= MAX_COLUMN && (c != ' ' && c != '\t')) {
+            line[i++] = '\n';
+            count = 0;
+        }
+
         line[i++] = c;
-        if ((line[i-1] != ' ' && line[i-1] != '\t') && (i%COLUMN)) {
+
+        if (c == '\t') {
+            count += 4;
+        } else {
+            count ++;
         }
     }
+    printf("%s", line);
 }
